c4e53c19d8480be0e35a1f271d7c50a9d7ec41af
김시내 <sinae@sinae-MacBookPro.local>
2022-10-17 14:57:31 +0900

    update

---
+++ medias/models.py
@@ -0,0 +1,46 @@
+from email.policy import default
+from django.db import models
+from common.models import CommonModel
+
+
+class Photo(CommonModel):
+    """Photo Model Definition"""
+
+    file = models.URLField()
+    description = models.CharField(
+        max_length=140,
+        blank=True,
+        default="",
+    )
+    rooom = models.ForeignKey(
+        "rooms.Room",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="photos",
+    )
+
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name="photos",
+    )
+
+    def __str__(self) -> str:
+        return "Photo File"
+
+
+class Video(CommonModel):
+    """Video Model Definition"""
+
+    file = models.URLField()
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+        related_name="videos",
+    )
+
+    def __str__(self) -> str:
+        return "Video File"
---
+++ rooms/serializers.py
@@ -0,0 +1,78 @@
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
+from .models import Amenity, Room
+from users.serializers import TinyUserSerializer
+from categories.serializers import CategorySerializer
+from reviews.serializers import ReviewSerializer
+from medias.serializers import PhotoSerializer
+from wishlists.models import Wishlist
+
+
+class AmenitySerializer(ModelSerializer):
+    class Meta:
+        model = Amenity
+        fields = (
+            "pk",
+            "name",
+            "description",
+        )
+
+
+class RoomDetailSerializer(ModelSerializer):
+    owner = TinyUserSerializer(
+        read_only=True,
+    )
+    amenities = AmenitySerializer(
+        read_only=True,
+        many=True,
+    )
+    category = CategorySerializer(
+        read_only=True,
+    )
+    rating = SerializerMethodField()
+    is_owner = SerializerMethodField()
+    is_liked = SerializerMethodField()
+    photos = PhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = "__all__"
+
+    def get_rating(self, room):
+        return room.rating()
+
+    def get_is_owner(self, room):
+        request = self.context.get("request")
+        if request:
+            return room.owner == request.user
+        return False
+
+    def get_is_liked(self, room):
+        request = self.context.get("request")
+        if request:
+            if request.user.is_authenticated:
+                return Wishlist.objects.filter(
+                    user=request.user, rooms__pk=room.pk
+                ).exists()
+        return False
+
+
+class RoomListSerializer(ModelSerializer):
+    is_owner = SerializerMethodField()
+    photos = PhotoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = (
+            "pk",
+            "name",
+            "country",
+            "city",
+            "price",
+            "rating",
+            "is_owner",
+            "photos",
+        )
+
+    def get_is_owner(self, room):
+        request = self.context["request"]
+        return room.owner == request.user
